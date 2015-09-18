c10d89af526fd08caa97746b7fbc4ffa26b45a48
Qzure Qin <azureplus@icloud.com>
2015-09-18 18:18:17 +0800

    fixed some bugs

---
+++ demo/src/demo.java
@@ -0,0 +1,23 @@
+import magick.ImageInfo;
+import magick.MagickImage;
+
+/**
+ * Created by dev on 15-9-18.
+ */
+public class demo
+{
+    static 
+    {
+        System.setProperty("jmagick.systemclassloader","false");
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        System.setProperty("java.library.path","/app/jmagick/lib");
+        System.out.println(System.getProperty("java.library.path"));
+        ImageInfo info;
+        MagickImage image;
+        info = new ImageInfo();
+        image = new MagickImage(new ImageInfo("/app/dev/jmagick/test/magicktest/pics.jpg"));
+    }
+}
